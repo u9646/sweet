505a375d2c176dd70293c67def6bd9e1087a5211
llllll <tokenl@163.com>
2022-12-14 09:58:57 +0000

    fix decrypt issue

--- pages/api/decrypt.js
+++ pages/api/decrypt.js
@@ -8,16 +8,16 @@ function WXBizDataCrypt(appId, sessionKey) {
 
 WXBizDataCrypt.prototype.decryptData = function (encryptedData, iv) {
   // base64 decode
-  var sessionKey = new Buffer(this.sessionKey, "base64");
-  encryptedData = new Buffer(encryptedData, "base64");
-  iv = new Buffer(iv, "base64");
+  var sessionKey = Buffer.from(this.sessionKey, "base64");
+  encryptedData = Buffer.from(encryptedData, "base64");
+  iv = Buffer.from(iv, "base64");
 
   try {
     // 解密
     var decipher = crypto.createDecipheriv("aes-128-cbc", sessionKey, iv);
     // 设置自动 padding 为 true，删除填充补位
     decipher.setAutoPadding(true);
-    var decoded = decipher.update(encryptedData, "binary", "utf8");
+    var decoded = decipher.update(encryptedData, "", "utf8");
     decoded += decipher.final("utf8");
 
     decoded = JSON.parse(decoded);
@@ -44,7 +44,7 @@ const getSessionKey = async (openid) => {
   }
   try {
     const client = await getRedisClient();
-    const { session_key } = JSON.parse(await client.get(query.openid));
+    const { session_key } = JSON.parse(await client.get(openid));
     return session_key;
   } catch (e) {
     return "";
